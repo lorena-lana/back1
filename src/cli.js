7fb43a30163d88f141a9c5b5ea85217b10d18100
lana <allana.rocha.delorera@gmail.com>
2024-03-20 14:46:53 -0300

    atualização quarta-feira 20/03

--- src/cli.js
+++ src/cli.js
@@ -1,31 +1,51 @@
-//comand line interface (cli)
-
-
+//Comand Line Interface (cli)
 
 import pegaArquivo from "./index.js";
-import fs from 'fs';
+
+import fs from "fs";
+
 import chalk from "chalk";
-//1)criar uma variável para recepcionar
+
+//1) criar uma variavel para recepcionar o caminho via terminal
 
 const caminho = process.argv;
-console.log(caminho);
+//console.log(caminho);
 
-//pegaArquivo(caminho[2]);
+// pegaArquivo(caminho[2]);
+
+function imprimeLista(resultado, identificador=""){
+    console.log(chalk.yellow('Lista de Links'),
+    chalk.black.bgGreen(identificador),
+    resultado);
+}
 
 async function processaTexto(argumentos){
     const caminho=argumentos[2];
+    try{
+        fs.statSync(caminho)
+    }
+    catch(erro){
+        if(erro.code==="ENOENT"){
+            console.log("Arquivo ou diretório não encontrado.");
+            return;
+        }
+    }
+
     if(fs.lstatSync(caminho).isFile()){
-const resultado =await pegaArquivo(caminho);
-console.log(chalk.yellow('lista de links'),resultado);
-}else if(fs.istatSyns(caminho).isDiretory()){
+        const resultado = await pegaArquivo(caminho);
+        // console.log(chalk.yellow('Lista de Links'), resultado);
+        imprimeLista(resultado);
+    }else if(fs.lstatSync(caminho).isDirectory()){
         const arquivos=await fs.promises.readdir(caminho);
-         arquivos.forEach(async(nomeDoArquivo)=>{
+        arquivos.forEach(async(nomeDoArquivo)=>{
             const lista=await pegaArquivo(`${caminho}/${nomeDoArquivo}`);
-            console.log(`${caminho}/${nomeDoArquivo}`);
-            console.log(lista);
-
+            //console.log(`${caminho}/${nomeDoArquivo}`);
+            //console.log(lista);
+            imprimeLista(lista,nomeDoArquivo);
         })
-}
+    }
+    
 
 }
+
 processaTexto(caminho);
