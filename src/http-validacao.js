2fa4c809891b732d52e9c988bed9982fb3feb9a7
lanalorena <com>
2024-03-26 14:43:01 -0300

    atualização dia 26/03

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,9 +1,14 @@
+import chalk from "chalk";
+
 async function checaStatus(listaURLs){
    const arrStatus= await Promise.all(
        listaURLs.map(async(url)=>{
+         try{
          const response = await fetch (url);
             return response.status;
-   
+         }catch(erro){
+            return manejaErros(erro);
+         }
    })
    )
    return arrStatus;
@@ -15,6 +20,15 @@ async function checaStatus(listaURLs){
         return arrLinks.map((objectLinks)=>Object.values(objectLinks).join());
  }
  
+
+ function manejaErros(erro){
+   //console.log(chalk.red("algo de errado"))
+   if(erro.cause.code==="ENOTFOUND"){
+      return "link não encontrado"
+   }else{
+      return "ocorreu um erro"
+   }
+ }
  
  //criar uma função listaValidada
 export default async function listaValidada(listaDeLinks){
@@ -22,5 +36,8 @@ export default async function listaValidada(listaDeLinks){
    const links = extraiLinks(listaDeLinks);
    const status= await checaStatus(links);
 // console.log(status);
-   return status;
+   return listaDeLinks.map((objeto,indece)=>({
+      ...objeto,
+      status:status[indece]
+   }));
 }
